9965c7c960b0172d981390b77a8cf57ae8c8d532
1200985 <1200985@isep.ipp.pt>
2021-05-09 22:34:56 +0100

    Added the development team.

--- src/main/java/app/ui/console/DevTeamUI.java
+++ src/main/java/app/ui/console/DevTeamUI.java
@@ -14,10 +14,11 @@ public class DevTeamUI implements Runnable{
     {
         System.out.println("\n");
         System.out.printf("Development Team:\n");
-        System.out.printf("\t Student Name 1 - 120XXXX@isep.ipp.pt \n");
-        System.out.printf("\t Student Name 2 - 120XXXX@isep.ipp.pt \n");
-        System.out.printf("\t Student Name 3 - 120XXXX@isep.ipp.pt \n");
-        System.out.printf("\t Student Name 4 - 120XXXX@isep.ipp.pt \n");
+        System.out.printf("\t Tomás Cancela - 1200985@isep.ipp.pt \n");
+        System.out.printf("\t Luís Moreira - 1200973@isep.ipp.pt \n");
+        System.out.printf("\t Diogo Gaspar - 120066@isep.ipp.pt \n");
+        System.out.printf("\t Bárbara Pinto - 1191507@isep.ipp.pt \n");
+        System.out.printf("\t João Pacheco - 1200996@isep.ipp.pt \n");
         System.out.println("\n");
     }
 }
