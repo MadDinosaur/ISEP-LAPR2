31e31b5e722b0ad2a31898df95c36d517ae00070
BarbaraDP <bdianapinto@outlook.com>
2021-05-09 16:51:25 +0100

    Revert bad commit.

--- src/main/java/app/controller/CreateEmployeeController.java
+++ src/main/java/app/controller/CreateEmployeeController.java
@@ -1,12 +1,31 @@
 package app.controller;
 
-public class CreateEmployeeController {
-    CreateEmployeeController() {
+import app.domain.model.Company;
+import app.domain.model.Employee;
 
+import java.util.List;
+
+public class CreateEmployeeController {
+    Company company;
+    Employee employee;
+
+    public CreateEmployeeController(Company company) {
+        this.company = company;
     }
-    String[] getOrganizationRoles() {}
-    void createEmployee(role) {}
-    void setEmployeeData(name, address, phoneNumber, email) {}
-    void setEmployeeData(name, address, phoneNumber, email, DIN) {}
-    void saveEmployee(employee) {}
+
+    public List<String> getOrganizationRoles() {
+        return company.getOrgRoleStore().getOrganizationRoles();
+    }
+
+    public void createEmployee(String role, String name, String address, String phoneNumber, String email, String soc) {
+        employee = company.getOrgRoleStore().getOrganizationRole(role).createEmployee(name, address, phoneNumber, email, soc);
+    }
+
+    public void createEmployee(String role, String name, String address, String phoneNumber, String email, String soc, String DIN) {
+        employee = company.getOrgRoleStore().getOrganizationRole(role).createEmployee(name, address, phoneNumber, email, soc, DIN);
+    }
+
+    public boolean saveEmployee() {
+        return company.getEmployeeStore().saveEmployee(employee);
+    }
 }
