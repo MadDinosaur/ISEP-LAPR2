dc500d1ecdbae02e89353a0b086f1c10d25517e6
1200985 <1200985@isep.ipp.pt>
2021-05-09 13:20:35 +0100

    Made way too many changes to document them all in this comment box.

--- src/main/java/app/ui/console/CreateTestTypeUI.java
+++ src/main/java/app/ui/console/CreateTestTypeUI.java
@@ -1,6 +1,8 @@
 package app.ui.console;
 
 import app.controller.CreateTestTypeController;
+import app.domain.model.Exceptions.InvalidCodeException;
+import app.domain.model.Exceptions.InvalidDescriptionException;
 
 import java.util.Scanner;
 
@@ -14,90 +16,133 @@ public class CreateTestTypeUI {
         createCollectionMethod();
         System.out.println("\n");
         System.out.println("--------Creating a new Test Type menu--------");
-        setDesignation();
+        setCode();
+        setTestTypeDescription();
         setCollectionMethod();
         setCategories();
         saveTestType();
         System.out.println("The test type you just created was saved!");
     }
 
-    public String setDesignation() {
-        boolean loop = false;
-        System.out.println("Insert the designation wanted for the test type:");
-        String designation = sc.nextLine();
-        System.out.printf("This is the designation you inserted: %s. Are you sure this is the designation you want? Yes/No\n", designation);
-        String confirmation = sc.nextLine();
-        while (!loop) {
-            if (confirmation.equalsIgnoreCase("yes")) {
-                loop = true;
-                cttc.setDesignation(sc.nextLine());
-                System.out.println("Designation saved!");
-            } else if (confirmation.equalsIgnoreCase("no")) {
-                System.out.println("Please, type the designation you want once again");
-                designation = sc.nextLine();
-                System.out.printf("This is the designation you inserted: %s. Are you sure this is the designation you want? Yes/No\n", designation);
-                confirmation = sc.nextLine();
-            } else {
-                System.out.println("Please insert either yes or no!");
-                confirmation = sc.nextLine();
+    public void setCode() {
+        try {
+            boolean loop = false;
+            System.out.println("Insert the code wanted for the test type:");
+            String code = sc.nextLine();
+            System.out.printf("This is the code you inserted: %s. Are you sure this is the code you want? Yes/No\n", code);
+            String confirmation = sc.nextLine();
+            while (!loop) {
+                if (confirmation.equalsIgnoreCase("yes")) {
+                    loop = true;
+                    cttc.setCode(sc.nextLine());
+                    System.out.println("Designation saved!");
+                } else if (confirmation.equalsIgnoreCase("no")) {
+                    System.out.println("Please, type the code you want once again");
+                    code = sc.nextLine();
+                    System.out.printf("This is the code you inserted: %s. Are you sure this is the code you want? Yes/No\n", code);
+                    confirmation = sc.nextLine();
+                } else {
+                    System.out.println("Please insert either yes or no!");
+                    confirmation = sc.nextLine();
+                }
             }
+        } catch (InvalidCodeException e) {
+            System.out.println("The designation shouldn't be any longer than 5 alphanumeric characters");
         }
-        return designation;
     }
 
-    public String setCollectionMethod() {
-        boolean loop = false;
-        System.out.println("Insert the description you want for the collection method of the test type:");
-        String description = sc.nextLine();
-        System.out.printf("This is the description you inserted: %s. Are you sure this is the description you want? (Yes/No)\n", description);
-        String confirmation = sc.nextLine();
-        while (!loop) {
-            if (confirmation.equalsIgnoreCase("yes")) {
-                loop = true;
-                cttc.setCollectionMethod(description);
-                cttc.setCollectionMethodToTestType();
-                System.out.println("Description saved!\n");
-            } else if (confirmation.equalsIgnoreCase("no")) {
-                System.out.println("Please, type the description you want once again.\n");
-                description = sc.nextLine();
-                System.out.printf("This is the description you inserted: \"%s\". Are you sure this is the description you want? (Yes/No)\n", description);
-                confirmation = sc.nextLine();
-            } else {
-                System.out.println("Please insert either yes or no!");
-                confirmation = sc.nextLine();
+    public void setCollectionMethod() {
+        try {
+            boolean loop = false;
+            System.out.println("Insert the description you want for the collection method of the test type:");
+            String description = sc.nextLine();
+            System.out.printf("This is the description you inserted: %s. Are you sure this is the description you want? (Yes/No)\n", description);
+            String confirmation = sc.nextLine();
+            while (!loop) {
+                if (confirmation.equalsIgnoreCase("yes")) {
+                    loop = true;
+                    cttc.setCollectionMethod(description);
+                    cttc.setCollectionMethodToTestType();
+                    System.out.println("Description saved!\n");
+                } else if (confirmation.equalsIgnoreCase("no")) {
+                    System.out.println("Please, type the description you want once again.\n");
+                    description = sc.nextLine();
+                    System.out.printf("This is the description you inserted: \"%s\". Are you sure this is the description you want? (Yes/No)\n", description);
+                    confirmation = sc.nextLine();
+                } else {
+                    System.out.println("Please insert either yes or no!");
+                    confirmation = sc.nextLine();
+                }
             }
+        } catch (InvalidDescriptionException e) {
+            System.out.println("The collection method's description shouldn't be any longer than 20 characters");
         }
-        return description;
     }
 
     public void setCategories() {
+        boolean noMore = false;
         boolean loop = false;
         String confirmation;
         System.out.println("Here is a list of the categories available:\n");
         cttc.displayCategoryList();
         System.out.println("One by one, insert the number of the category you want to associate to the test type (If you don't want to insert any more categories, please press ENTER)\n");
         String indexPedido = sc.nextLine();
-        while (!loop) {
+        while (!noMore) {
             if (indexPedido.equals("")) {
-                loop = true;
-            } else {
-                System.out.printf("Are you sure this is the category you want? (Yes/No)\n");
-                confirmation = sc.nextLine();
-                if (confirmation.equalsIgnoreCase("yes")) {
-                    cttc.setCategoriesToTestType(Integer.parseInt(indexPedido));
-                    cttc.displayCategoryList();
-                    System.out.println("Please insert another category (If you don't want to insert any more categories, please press ENTER)");
-                    indexPedido = sc.nextLine();
-                } else if (confirmation.equalsIgnoreCase("no")) {
-                    System.out.println("Please, choose another category");
-                    cttc.displayCategoryList();
-                    indexPedido = sc.nextLine();
-                } else {
+                noMore = true;
+            } else if (Integer.parseInt(indexPedido) < 0 || Integer.parseInt(indexPedido) > (cttc.getCategoryListSize() - 1) ) {
                     System.out.println("Please insert a valid category");
                     cttc.displayCategoryList();
                     indexPedido = sc.nextLine();
+            } else {
+                System.out.printf("Are you sure this is the category you want? (Yes/No)\n");
+                confirmation = sc.nextLine();
+                while (!loop) {
+                    if (confirmation.equalsIgnoreCase("yes")) {
+                        loop = true;
+                        cttc.setCategoriesToTestType(Integer.parseInt(indexPedido));
+                        cttc.displayCategoryList();
+                        System.out.println("Please insert another category (If you don't want to insert any more categories, please press ENTER)");
+                        indexPedido = sc.nextLine();
+                    } else if (confirmation.equalsIgnoreCase("no")) {
+                        System.out.println("Please, choose another category");
+                        cttc.displayCategoryList();
+                        indexPedido = sc.nextLine();
+                        System.out.printf("Are you sure this is the category you want? (Yes/No)\n");
+                        confirmation = sc.nextLine();
+                    } else {
+                        System.out.println("Please insert either yes or no!");
+                        confirmation = sc.nextLine();
+                    }
                 }
             }
+        }
+    }
+
+    public void setTestTypeDescription() {
+        try {
+            boolean loop = false;
+            System.out.println("Insert the description you want for the test type:");
+            String description = sc.nextLine();
+            System.out.printf("This is the description you inserted: %s. Are you sure this is the description you want? (Yes/No)\n", description);
+            String confirmation = sc.nextLine();
+            while (!loop) {
+                if (confirmation.equalsIgnoreCase("yes")) {
+                    loop = true;
+                    cttc.setDescription(description);
+                    System.out.println("Description saved!\n");
+                } else if (confirmation.equalsIgnoreCase("no")) {
+                    System.out.println("Please, type the description you want once again.\n");
+                    description = sc.nextLine();
+                    System.out.printf("This is the description you inserted: \"%s\". Are you sure this is the description you want? (Yes/No)\n", description);
+                    confirmation = sc.nextLine();
+                } else {
+                    System.out.println("Please insert either yes or no!");
+                    confirmation = sc.nextLine();
+                }
+            }
+        } catch (InvalidDescriptionException e) {
+            System.out.println("The description you introduced shouldn't be any longer than 15 characters.");
         }
     }
 
