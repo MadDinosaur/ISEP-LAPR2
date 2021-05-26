e2cccebd9abf6b93874119f5399c69dec33bac0b
1200996 <1200996@isep.ipp.pt>
2021-05-26 19:17:54 +0100

    Test class changes

--- src/main/java/app/domain/model/Test.java
+++ src/main/java/app/domain/model/Test.java
@@ -1,15 +1,58 @@
 package app.domain.model;
 
+
+import app.domain.store.TestParamStore;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class Test {
-    private String code;
-
+    private Client client;
+    private List<Category> listOfCategories;
+    private String testCode;
     private String nhsCode;
-
-    private Date createdAt;
-
+    private Date dateOfCreation;
+    private List<Parameter> listOfParameters = new ArrayList<>();
     private TestParamStore testParamStore;
+    private int stateOfTest;
+
+    private enum StateOfTest{
+        REGISTERED,
+        //...
+    }
+    public Test(Client client, List<Category> listOfCategories,String testCode, String nhsCode){
+        setClient(client);
+        setListOfCategories(listOfCategories);
+        setTestCode(testCode);
+        setNhsCode(nhsCode);
+        getParametersFromCategoriesToStore();
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+    public void setListOfCategories(List<Category> listOfCategories) {
+        this.listOfCategories = listOfCategories;
+    }
+    public void setTestCode(String testCode) {
+        this.testCode = testCode;
+    }
+    public void setNhsCode(String nhsCode) {
+        this.nhsCode = nhsCode;
+    }
+
+    private void getParametersFromCategoriesToStore(){
+        for(int i = 0; i<this.listOfCategories.size();i++){
+            Category category = listOfCategories.get(i);
+            List<Parameter> listOfParametersOfCategory = category.getParameterList();
+            for (int j = 0; j<listOfParametersOfCategory.size(); j++) {
+                Parameter parameter = listOfParametersOfCategory.get(i);
+                listOfParameters.add(parameter);
+                //a testParamStore não tem metodo de add para a store ainda
+            }
+        }
+    }
+
 }
+
