48a6a218df65108a209fa4c5a330708d8559c4ce
BarbaraDP <bdianapinto@outlook.com>
2021-05-28 09:46:58 +0100

    Implemented Parameter DTO.

---
+++ src/main/java/app/mappers/dto/ParamDTO.java
@@ -0,0 +1,14 @@
+package app.mappers.dto;
+
+public class ParamDTO {
+    String code;
+    String name;
+    String description;
+
+
+    public ParamDTO(String code, String name, String description) {
+        this.code = code;
+        this.name = name;
+        this.description = description;
+    }
+}
