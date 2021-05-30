e57bd5b0eec8f2b820d2c0f8f8bba509fe593367
1200985 <1200985@isep.ipp.pt>
2021-05-30 18:46:59 +0100

    Made changes to the artifacts. Made changes to the code.

--- src/main/java/app/domain/shared/Constants.java
+++ src/main/java/app/domain/shared/Constants.java
@@ -10,4 +10,6 @@ public class Constants {
 
     public static final String PARAMS_FILENAME = "config.properties";
     public static final String PARAMS_COMPANY_DESIGNATION = "Company.Designation";
+
+    public static final int REPORT_MAX_WORDS = 400;
 }
