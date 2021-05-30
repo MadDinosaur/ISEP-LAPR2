03096e03d58077c4c3caec7b0425eec26a7f95b4
João Pacheco <1200996@isep.ipp.pt>
2021-05-30 19:23:47 +0100

    no message

--- src/test/java/app/domain/model/CompanyTest.java
+++ src/test/java/app/domain/model/CompanyTest.java
@@ -13,13 +13,8 @@ public class CompanyTest extends TestCase {
     private EmployeeStore employeeStore = new EmployeeStore();
     private Client client = new Client("test",(long)  8765432187654322.0,1234512345,new DateBirth(24,12,2002),1234512347,(long)12345123456.0,new Email("teste@gmail.com"),"male");
     private String pss = "1234567890";
-    private Employee employee = new Employee("testE",new OrganizationRole("oi","teste"),"testeEmp","house","testea@gmai.com","12345678912","1234");
 
-/*    public void testSaveEmployeeAsUser() {
-        employeeStore.saveEmployee(employee);
-        Assert.assertEquals(employee.getName(),employeeStore.);
-    }*/
-
+    
     public void testSaveClientAsUser() {
         clientStore.saveClient(client);
         Client client2 = clientStore.getClientByCardNumber( (long)8765432187654322.0);
