4aa481800b3e15a5e282db6bb5b27de69351a864
1200985 <1200985@isep.ipp.pt>
2021-06-16 11:32:13 +0100

    Revert "Tried fixing an alive mutant. might revert later."
    This reverts commit f57d911d059829417c6d68e905e35b89f104f834.

--- src/main/java/app/domain/model/Company.java
+++ src/main/java/app/domain/model/Company.java
@@ -96,6 +96,8 @@ public class Company {
     }
 
     public boolean validateCategory(Category pc) {
+        if (pc == null)
+            return false;
         return !this.categoryList.contains(pc);
     }
 
